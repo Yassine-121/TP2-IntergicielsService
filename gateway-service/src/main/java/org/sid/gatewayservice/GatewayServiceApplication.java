047e83ad1d250d3634f278d17ee123354bee2285
Yassine-121 <lyazighi.yassine@gmail.com>
2021-09-10 22:46:01 +0100

    Gateway Service

---
+++ gateway-service/src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,23 @@
+package org.sid.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	RouteLocator routeLocator(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route((r)->r.path("/customers/**").uri("http://localhost:8081/"))
+				.route((r)->r.path("/products/**").uri("http://localhost:8082/"))
+				.build();
+	}
+}
